d27d157e09280626dbd89f06ea2b38ce3c8d0325
vuhongbao99 <baobao280799@gmail.com>
2025-09-26 13:57:23 +0700

    first commit

---
+++ BE_SMRS_FALL25/src/main/java/com/example/be_smrs_fall25/BeSmrsFall25Application.java
@@ -0,0 +1,13 @@
+package com.example.be_smrs_fall25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeSmrsFall25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeSmrsFall25Application.class, args);
+    }
+
+}
