15500db98e584208f330d6ef3e22c8d008bd9626
Vu Bao <vu.bao@example.com>
2025-09-26 13:57:23 +0700

    first commit

---
+++ BE_SMRS_FALL25/src/test/java/com/example/be_smrs_fall25/BeSmrsFall25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_smrs_fall25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeSmrsFall25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
