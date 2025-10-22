0e336d13b78d6b4b00092aa8cbb025b9f959f674
vuhongbao99 <vuhongbao280799@gmail.com>
2025-10-22 19:22:42 +0700

    Update thanh cong SendEmail

---
+++ smrs-service/src/main/java/com/example/smrsservice/service/MailService.java
@@ -0,0 +1,22 @@
+package com.example.smrsservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendSimpleMail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("baobao280799@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
