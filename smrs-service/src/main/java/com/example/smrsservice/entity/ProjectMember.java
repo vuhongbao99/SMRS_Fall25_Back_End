193f6accb1c8b4eec7d894ee8c21d97498b5db5d
vuhongbao99 <vuhongbao280799@gmail.com>
2025-10-28 22:27:01 +0700

    Chinh sua lai changePassword

--- smrs-service/src/main/java/com/example/smrsservice/entity/ProjectMember.java
+++ smrs-service/src/main/java/com/example/smrsservice/entity/ProjectMember.java
@@ -26,5 +26,13 @@ public class ProjectMember {
     @JoinColumn(name = "account_id")
     private Account account;
 
-    private String status = "Pending";
+    private String status = "Pending"; // pending  , approve thì tức là nó đồng vo nhóm , camcel  // compeple
+
+//    khi mà thằng user nó change status của cái bảng ny pending => chuyển status từ pending sang approve hoặc là cancel
+//    // api user approve  id cả thagnừ git all trong bảng này  id / project id  get by theo account vs proj  =>  em chuyển status cảu bẳng sang approve nếu mà cái này đang approve thì không chuyển lại cencel \
+//    => get all bảng này theo account Id  tât cả cái recỏd liên account id này  vs trạng thái pending  chueyẻn thành cacnel hết
+//    => project hoàng hành
+
+//    bảng ađ thằng user em muối mời zo trồng project  ( email, id project )  email account  tại thời đó em cmail service em gửi maill tới cái email của thằng đó
+
 }
---
+++ smrs-service/src/main/java/com/example/smrsservice/service/UploadServic.java
@@ -0,0 +1,26 @@
+package com.example.smrsservice.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class UploadServic {
+
+    private final Cloudinary cloudinary;
+
+    public String uploadImage(MultipartFile file) {
+        try {
+            Map<?, ?> uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
+            return uploadResult.get("secure_url").toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Lỗi upload hình ảnh lên Cloudinary", e);
+        }
+    }
+}
