c1aee22023e0ef1d7ae7c7a31da930ba02158946
vuhongbao99 <vuhongbao280799@gmail.com>
2025-11-05 15:26:00 +0700

    update lại entity và service task

--- smrs-service/src/main/java/com/example/smrsservice/dto/task/TaskResponse.java
+++ smrs-service/src/main/java/com/example/smrsservice/dto/task/TaskResponse.java
@@ -12,16 +12,18 @@ public class TaskResponse {
     private Integer id;
     private String name;
     private String description;
-
     private AccountShortDto createdBy;
     private AccountShortDto assignedTo;
-
     private Date startDate;
     private Date deadline;
-
     private Double progressPercent;
     private String status;
 
+    // Milestone info
     private Integer milestoneId;
     private String milestoneName;
+
+    // Project info
+    private Integer projectId;
+    private String projectName;
 }
