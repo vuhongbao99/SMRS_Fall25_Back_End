8084ef6d87d352da9ef4e010274499ceb2dfd394
vuhongbao99 <vuhongbao280799@gmail.com>
2025-11-07 18:09:37 +0700

    fix lại filter cho Project Enum

--- smrs-service/src/main/java/com/example/smrsservice/entity/ProjectMember.java
+++ smrs-service/src/main/java/com/example/smrsservice/entity/ProjectMember.java
@@ -32,13 +32,5 @@ public class ProjectMember {
     private String memberRole; // "TEACHER", "STUDENT"
 
 
-    // pending  , approve thì tức là nó đồng vo nhóm , camcel  // compeple
-
-//    khi mà thằng user nó change status của cái bảng ny pending => chuyển status từ pending sang approve hoặc là cancel
-//    // api user approve  id cả thagnừ git all trong bảng này  id / project id  get by theo account vs proj  =>  em chuyển status cảu bẳng sang approve nếu mà cái này đang approve thì không chuyển lại cencel \
-//    => get all bảng này theo account Id  tât cả cái recỏd liên account id này  vs trạng thái pending  chueyẻn thành cacnel hết
-//    => project hoàng hành
-
-//    bảng ađ thằng user em muối mời zo trồng project  ( email, id project )  email account  tại thời đó em cmail service em gửi maill tới cái email của thằng đó
 
 }
