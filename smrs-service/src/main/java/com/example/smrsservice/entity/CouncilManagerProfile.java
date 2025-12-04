d516dadcf7afe68cdf543ad63a6673a462cf409b
vuhongbao99 <baobao280799@gmail.com>
2025-12-05 00:59:39 +0700

    thay ddooir

--- smrs-service/src/main/java/com/example/smrsservice/entity/CouncilManagerProfile.java
+++ smrs-service/src/main/java/com/example/smrsservice/entity/CouncilManagerProfile.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CouncilManagerProfile {
+public class    CouncilManagerProfile {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
