b5226803df36195c9e6d2b7bcaa465bc98a59e48
vuhongbao99 <baobao280799@gmail.com>
2025-12-25 00:42:15 +0700

    update

--- smrs-service/src/main/java/com/example/smrsservice/repository/CouncilManagerProfileRepository.java
+++ smrs-service/src/main/java/com/example/smrsservice/repository/CouncilManagerProfileRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface CouncilManagerProfileRepository extends JpaRepository<CouncilManagerProfile,Integer> {
+public interface    CouncilManagerProfileRepository extends JpaRepository<CouncilManagerProfile,Integer> {
     Optional<CouncilManagerProfile> findByAccountId(Integer accountId);
 
     Optional<CouncilManagerProfile> findByEmployeeCode(String employeeCode);
